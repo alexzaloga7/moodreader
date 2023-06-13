e37d675be677920ca5538e2cc1d822044adb1e5e
tarawiedeman <33428501+tarawiedeman@users.noreply.github.com>
2023-06-13 16:05:03 -0700

    redirect to mood gallery working, name loading not

--- src/Page/LandingPage/LandingPage.tsx
+++ src/Page/LandingPage/LandingPage.tsx
@@ -1,11 +1,32 @@
 import "../LandingPage/LandingPage.scss";
+import {useNavigate} from "react-router-dom";
+
+
 
 const LandingPage: React.FC = () => {
+
+
+  const navigate = useNavigate();
   return (
     <div className="landing-page">
       <h1>Welcome to Mood Reader! Made by Alex & Tara</h1>
+      <form onSubmit={PageChange}>
+        <input
+        type="text"
+        placeholder="Enter your name"
+        name="name"/>
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
+
+  function PageChange(event: React.FormEvent){
+    event.preventDefault();
+    const form = event.currentTarget as HTMLFormElement;
+    const name = form.elements.namedItem("name") as HTMLInputElement;
+    navigate(`/moodgallery?name=${name.value}`);
+  
+  };
 };
 
 export default LandingPage;
--- src/Page/MoodGallery/MoodGallery.tsx
+++ src/Page/MoodGallery/MoodGallery.tsx
@@ -1,9 +1,19 @@
 import "../MoodGallery/MoodGallery.scss";
+import {useState} from 'react';
+import {useParams} from "react-router-dom";
 
 const MoodGallery: React.FC = () => {
+
+  const params = useParams<{name: string }>();
+  const name=params.name;
+  const [userName]=useState(name);
+
+
+  
+
   return (
     <div className="mood-gallery">
-      <h1>Welcome to my app!</h1>
+      <h1>Welcome {userName}</h1>
     </div>
   );
 };
