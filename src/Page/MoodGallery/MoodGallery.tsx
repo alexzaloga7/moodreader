f46678cd4b5b65653b4a50e3213b8601971e8cf2
Alex Zaloga <alexandrazaloga@gmail.com>
2023-06-27 20:39:05 -0400

    updated image names

--- src/Page/MoodGallery/MoodGallery.tsx
+++ src/Page/MoodGallery/MoodGallery.tsx
@@ -1,6 +1,5 @@
 import { useNavigate, useLocation } from "react-router-dom";
 import "./MoodGallery.scss";
-import Heart from "../../Assets/Images/heart.png";
 import Balance from "../../Assets/Images/moodbalance.png";
 import Focus from "../../Assets/Images/focus.png";
 import Relax from "../../Assets/Images/relax.png";
@@ -23,46 +22,55 @@ const MoodGallery: React.FC = () => {
   return (
     <div className="mood-gallery">
       <div className="headercontainer">
-      <h1 className="mood-header">Welcome {name}</h1>
-      <img src={Balance} alt="balance" className="main-image"></img>
+        <h1 className="mood-header">Welcome {name}</h1>
+        <img src={Balance} alt="balance" className="main-image"></img>
       </div>
       <h2 className="mood-subheader"> What do you want to achieve?</h2>
       <div className="mood-options">
-        
         <button
           className="mood-button"
           id="focus"
           onClick={() => moodHandler("focus")}
-        >Focus<img src={Focus} alt="focus" className="icon"></img>
-          <p className="mood-description">Channel your energy towards a singular task or objective.</p>
+        >
+          Focus<img src={Focus} alt="focus" className="icon"></img>
+          <p className="mood-description">
+            Channel your energy towards a singular task or objective.
+          </p>
         </button>
-  
+
         <button
           className="mood-button"
           id="relax"
           onClick={() => moodHandler("relax")}
-        >Relax <img src={Relax} alt="relax" className="icon"></img><p className="mood-description">Practice deep breathing to release tension and promote relaxation.</p>
-
+        >
+          Relax <img src={Relax} alt="relax" className="icon"></img>
+          <p className="mood-description">
+            Practice deep breathing to release tension and promote relaxation.
+          </p>
         </button>
 
         <button
           className="mood-button"
           id="energize"
           onClick={() => moodHandler("energize")}
-        >Energize <img src={Revitalize} alt="revitalize" className="icon"></img>
-          <p className="mood-description">Replenish vitality and regain vigor.</p>
-
+        >
+          Energize{" "}
+          <img src={Revitalize} alt="revitalize" className="icon"></img>
+          <p className="mood-description">
+            Replenish vitality and regain vigor.
+          </p>
         </button>
 
         <button
           className="mood-button"
           id="balance"
           onClick={() => moodHandler("balance")}
-        >Balance <img src={Equilibrium} alt="balance" className="icon"></img>
-          <p className="mood-description">Restore equilibrium and regain a sense of stability.</p>
-
+        >
+          Balance <img src={Equilibrium} alt="balance" className="icon"></img>
+          <p className="mood-description">
+            Restore equilibrium and regain a sense of stability.
+          </p>
         </button>
-        
       </div>
     </div>
   );
