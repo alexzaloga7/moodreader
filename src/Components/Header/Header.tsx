12e510bff7981b1336d72c4a1ef105ff6d113dea
Alex Zaloga <alexandrazaloga@gmail.com>
2023-08-09 19:24:20 -0400

    updated lowercase image naming

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,11 +1,14 @@
 import "../Header/Header.scss";
-import logo from "../../Assets/Images/MM.png";
+import logo from "../../Assets/Images/mm.png";
 import { NavLink } from "react-router-dom";
 
 const Header: React.FC = () => {
   return (
     <div className="header">
-     <NavLink to="/"> <img src={logo} className="logo" alt="logo"></img></NavLink>
+      <NavLink to="/">
+        {" "}
+        <img src={logo} className="logo" alt="logo"></img>
+      </NavLink>
     </div>
   );
 };
